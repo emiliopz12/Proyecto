57f4d7f21ed83f1b1e8ea51e52039f86fd09e7eb
emiliopz12 <12epz12@gmail.com>
2016-03-29 19:15:04 -0600

    prueba

--- app/src/main/java/com/example/horse/proyecto/actividadPrincipal.java
+++ app/src/main/java/com/example/horse/proyecto/actividadPrincipal.java
@@ -11,6 +11,7 @@ public class actividadPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_principal);
+
     }
 
     @Override
