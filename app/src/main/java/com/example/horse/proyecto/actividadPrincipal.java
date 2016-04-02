6b35359b5f99eab81a783f8525c4e9c2837785e6
emiliopz12 <12epz12@gmail.com>
2016-04-02 16:29:02 -0600

    prueba

--- app/src/main/java/com/example/horse/proyecto/actividadPrincipal.java
+++ app/src/main/java/com/example/horse/proyecto/actividadPrincipal.java
@@ -4,13 +4,44 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class actividadPrincipal extends AppCompatActivity {
+
+    TabHost TbH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_principal);
+
+
+
+
+        TbH = (TabHost) findViewById(R.id.tabHost); //llamamos al Tabhost
+        TbH.setup();
+
+        TabHost.TabSpec inicio = TbH.newTabSpec("Inicio");  //aspectos de cada Tab (pestaña)
+        TabHost.TabSpec cuenta = TbH.newTabSpec("Mi cuenta");
+        TabHost.TabSpec reporte = TbH.newTabSpec("Realizar Reporte");
+        TabHost.TabSpec mapa = TbH.newTabSpec("Mapa");
+
+        inicio.setIndicator("Inicio");    //qué queremos que aparezca en las pestañas
+        inicio.setContent(R.id.tab1); //definimos el id de cada Tab (pestaña)
+
+        cuenta.setIndicator("Mi cuenta");
+        cuenta.setContent(R.id.tab2);
+
+        reporte.setIndicator("Realizar Reporte");
+        reporte.setContent(R.id.tab3);
+
+        mapa.setIndicator("Mapa");
+        mapa.setContent(R.id.tab4);
+
+        TbH.addTab(inicio); //añadimos los tabs ya programados
+        TbH.addTab(cuenta);
+        TbH.addTab(reporte);
+        TbH.addTab(mapa);
 
     }
 
