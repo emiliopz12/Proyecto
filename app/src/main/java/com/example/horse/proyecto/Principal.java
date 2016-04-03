344d938d81e4a8da8ea3c497de1ac73e009e28c2
emiliopz12 <12epz12@gmail.com>
2016-04-03 11:31:58 -0600

    principal

--- app/src/main/java/com/example/horse/proyecto/Principal.java
+++ app/src/main/java/com/example/horse/proyecto/Principal.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.Locale;
 
@@ -117,8 +118,8 @@ public class Principal extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_principal, container, false);
-          //  TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-          //  textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            TextView textView = (TextView) rootView.findViewById(R.id.textView);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
