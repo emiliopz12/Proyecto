63fb3da2acf851ebde1c90ebe6dc91c92858b6c8
danelhe <danelhe@gmail.com>
2016-04-08 15:03:40 -0600

    ...

---
+++ app/src/main/java/com/example/horse/proyecto/Representacion.java
@@ -0,0 +1,14 @@
+package com.example.horse.proyecto;
+
+public class Representacion {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
