62bda9b24073f0079aa476698953f36d02a2d76f
emiliopz12 <12epz12@gmail.com>
2016-04-13 23:00:26 -0600

    principal

--- app/src/main/java/com/example/horse/proyecto/Principal.java
+++ app/src/main/java/com/example/horse/proyecto/Principal.java
@@ -1,26 +1,32 @@
 package com.example.horse.proyecto;
 
-import android.os.AsyncTask;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -42,9 +48,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -54,6 +62,7 @@ public class Principal extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private EditText inputPelicula;
+    public static FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +73,7 @@ public class Principal extends AppCompatActivity {
         //setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), this, this);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
@@ -73,13 +82,15 @@ public class Principal extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                REST();
+                //REST();
+                onLaunchCamera();
             }
         });
+
 
 
     }//-------------------------------------------------------------------FIN ONCREATE
@@ -116,7 +127,7 @@ public class Principal extends AppCompatActivity {
 
 
 
-            /*for (int i=0;i<proveedores.length();i++){
+            for (int i=0;i<proveedores.length();i++){
                 JSONObject json = proveedores.getJSONObject(i);
                 Map<String,String> map = new HashMap<>(2);
                 map.put("id",json.getString("id"));
@@ -126,7 +137,7 @@ public class Principal extends AppCompatActivity {
             SimpleAdapter adapter = new SimpleAdapter(this,data,android.R.layout.simple_list_item_2,
                     new String[]{"id","fecha"},new int[]{android.R.id.text1,android.R.id.text2});
             ListView listaVisual = (ListView) findViewById(R.id.listView);
-            listaVisual.setAdapter(adapter);*/
+            listaVisual.setAdapter(adapter);
         }catch(JSONException e){
             e.printStackTrace();
             Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
@@ -163,6 +174,82 @@ public class Principal extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+/*    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent imageCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(imageCaptureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+           // mImageView.setImageBitmap(imageBitmap);
+        }
+    }*/
+
+    public final String APP_TAG = "MyCustomApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+
+    public void onLaunchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName)); // set the image file name
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri takenPhotoUri = getPhotoFileUri(photoFileName);
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+                // Load the taken image into a preview
+               // ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
+               // ivPreview.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the Uri for a photo stored on disk given the fileName
+    public Uri getPhotoFileUri(String fileName) {
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+            // Get safe storage directory for photos
+            // Use `getExternalFilesDir` on Context to access package-specific directories.
+            // This way, we don't need to request external read/write runtime permissions.
+            File mediaStorageDir = new File(
+                    getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+        }
+        return null;
+    }
+
+    // Returns true if external storage for photos is available
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        return state.equals(Environment.MEDIA_MOUNTED);
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -171,22 +258,30 @@ public class Principal extends AppCompatActivity {
          * The fragment argument representing the section number for this
          * fragment.
          */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
+        private static String ARG_SECTION_NUMBER = "section_number";
+         static Activity p;
+         static Principal a;
+        private List<Reporte> reportes = new ArrayList<Reporte>();
+        static final int REQUEST_IMAGE_CAPTURE = 1;
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
+        public static PlaceholderFragment newInstance(int sectionNumber, Activity pr, Principal pp) {
             PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
+            p = pr;
+            a = pp;
+
+           Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
+           fragment.setArguments(args);
             return fragment;
         }
+
+        public PlaceholderFragment() {
+        }
+
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -198,18 +293,58 @@ public class Principal extends AppCompatActivity {
                 rootView = inflater.inflate(R.layout.inicio, container, false);
 
                 //HACER LO QUE TENGA QUE VER CON INICIO
+                reportes.add(new Reporte("Seguridad", "robo", "san jose", "12/12/12"));
+
+
+                ArrayAdapter<Reporte> adapter = new AdaptadorReporte(p, reportes);
+                ListView list = (ListView) rootView.findViewById(R.id.listaReportes);
+                list.setAdapter(adapter);
+
+                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View viewClicked,
+                                            int position, long id) {
+                        //Car clickedCar = myCars.get(position);
+                        String message = "Elegiste item No. " + (1 + position);
+                        Toast.makeText(p, message,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                //fab.setVisibility(View.VISIBLE);
             }
 
             if(getArguments().getInt(ARG_SECTION_NUMBER) == 2){
                 rootView = inflater.inflate(R.layout.cuenta, container, false);
 
                 //HACER  LO QUE TENGA QUE VER CON CUENTA
+
+                //a.dispatchTakePictureIntent();
+
             }
 
             if(getArguments().getInt(ARG_SECTION_NUMBER) == 3){
-                rootView = inflater.inflate(R.layout.reporte, container, false);
+                rootView = inflater.inflate(R.layout.cuenta, container, false);
 
                 //HACER  LO QUE TENGA QUE VER CON REPORTE
+
+       /*         ImageButton imagen = (ImageButton) rootView.findViewById(R.id.imageButton);
+
+                imagen.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+
+                    public void onClick(View arg0) {
+                        //a.onLaunchCamera();
+
+                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+
+                    }
+                });*/
+
+               // fab.setVisibility(View.INVISIBLE);
             }
 
             //TextView textView = (TextView) rootView.findViewById(R.id.textView);
@@ -219,7 +354,7 @@ public class Principal extends AppCompatActivity {
         }
     }
 
-    public class MapFragment extends Fragment {
+    public static class MapFragment extends Fragment {
 
         MapView mMapView;
         private GoogleMap googleMap;
@@ -296,8 +431,13 @@ public class Principal extends AppCompatActivity {
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        Activity p;
+        Principal pp;
+
+        public SectionsPagerAdapter(FragmentManager fm, Activity p, Principal pp) {
             super(fm);
+            this.p = p;
+            this.pp = pp;
         }
 
         @Override
@@ -305,12 +445,10 @@ public class Principal extends AppCompatActivity {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
             //return PlaceholderFragment.newInstance(position + 1);
-
             if(position == 3){
                 return new MapFragment();
             }
-            return PlaceholderFragment.newInstance(position + 1);
-
+            return PlaceholderFragment.newInstance(position + 1, p, pp);
         }
 
         @Override
