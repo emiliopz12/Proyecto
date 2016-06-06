fcf16d2e0e9863051cc68af37aa1bcd6baecd350
emiliopz12 <12epz12@gmail.com>
2016-06-05 18:10:37 -0600

    principal

--- app/src/main/java/com/example/horse/proyecto/Imagen.java
+++ app/src/main/java/com/example/horse/proyecto/Imagen.java
@@ -1,10 +1,12 @@
 package com.example.horse.proyecto;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 
 public class Imagen extends AppCompatActivity {
 
@@ -15,14 +17,29 @@ public class Imagen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagen);
 
+        Hilo01 MiHilo01 = new Hilo01();
+        MiHilo01.execute();
+
+        Toast.makeText(getApplicationContext(), "Cargando imagen...", Toast.LENGTH_SHORT).show();
+
+        ImageView imagen = (ImageView) findViewById(R.id.imagenFoto);
+
+
+        imagen.setImageResource(R.drawable.sincargar);
+
+        findViewById(R.id.volver).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                volver();
+            }
+        });
 
 
 
-        while(FTP.FOTO == null){
-            Toast.makeText(getApplicationContext(), "Cargando...", Toast.LENGTH_SHORT).show();
-        }
 
+    }
 
+    public void cargar(){
         ImageView imagen = (ImageView) findViewById(R.id.imagenFoto);
 
         imagen.setImageBitmap(FTP.FOTO);
@@ -33,12 +50,80 @@ public class Imagen extends AppCompatActivity {
                 volver();
             }
         });
-
-
     }
 
     public void volver(){
+        FTP.FOTO = null;
         this.finish();
         super.onBackPressed();
     }
+
+
+    private class Hilo01 extends AsyncTask<Void,Integer,Boolean> {
+        // Parametro 1 (Void) es para doInBackground
+// Parametro 2 (Integer) es para onProgressUpdate
+// Parametro 3 (Boolean) tipo de respuesta del doInBackground!!
+//Usted los puede cambiar como guste!!
+
+
+        // Coloque aquí las tareas que deben hacerse (en el hilo principal) antes de enviar el hilo a trabajar en segundo plano
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Iniciacion
+
+
+        }
+
+        // Tareas que se ejecutaran en segundo plano
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // Tareas ejecutadas en segundo plano
+
+            // Puede llamar al metodo publishProgress que permite comunicarse con el hilo principal
+            // Ejemplo: publishProgress(10); // que enviaria 10 a onProgressUpdate
+
+
+            while(FTP.FOTO == null){
+                publishProgress(1);
+            }
+
+
+            return true;
+        }
+
+        // Se ejecuta cuando se llama a publishProgress. Recibe parametros enviados desde doInBackground
+        // y que pueden ser usado para notificar/actualizar el estado del proceso en segundo plano
+        // Puede ser un valor o una cadena de valores
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            //super.onProgressUpdate(values);
+
+
+        }
+
+
+        // Una vez que el hilo en segundo plano termina, se ejecuta lo indicado en el siguiente metodo
+        @Override
+        protected void onPostExecute(Boolean resultado) {
+            //super.onPostExecute(aVoid);
+
+            ImageView imagen = (ImageView) findViewById(R.id.imagenFoto);
+
+            imagen.setImageBitmap(FTP.FOTO);
+
+
+
+
+        }
+        // Si se corta la ejecución del hilo, se ejecutan las instrucciones indicadas en el siguiente método
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+
+
+        }
+    }
+
+
 }
